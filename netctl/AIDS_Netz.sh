5654a984adc7db19693f7bb8cfacb40266c8b828
Philipp Schmitt <philipp@schmitt.co>
2013-10-16 15:11:09 +0200

    Updated pia-tools and netctl hooks

--- netctl/AIDS_Netz.sh
+++ netctl/AIDS_Netz.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/zsh
 
 if [[ "$1" == "up" ]]; then
-#systemctl start dropbox@pschmitt insync@pschmitt nfsd
+    systemctl start dropbox@pschmitt insync@pschmitt nfsd
     mount /mnt/nfs-raid
 elif [[ "$1" == "down" ]]; then
-#systemctl stop dropbox@pschmitt insync@pschmitt nfsd
+    systemctl stop dropbox@pschmitt insync@pschmitt nfsd
     killall insync
     umount /mnt/nfs-raid
 fi
